71a2596b5e37eaaaac210bd75a244adfdf1fe966
aeganskies <mansur.rahman.5974@student.uu.se>
2023-01-11 22:34:05 +0100

    Refactoring to split with notebook

--- main.py
+++ main.py
@@ -44,11 +44,8 @@ def getAllSegments():
 
 def cleanData():
     apData = pd.read_csv('./Data/hnetData.csv')
-
     apData = cleanAndConvertToNum(apData)
-
-    apData.to_csv('./Data/CleanHnetData.csv', index=False)
-
+    apData.to_csv('./Data/RawDataFile.csv', index=False)
 
 def preProcess():
     # Step 1.1 - Get all data from hNet
@@ -57,55 +54,9 @@ def preProcess():
     # Step 1.2 - Clean data with formatting and output to new csv file
     cleanData()
 
-    # Step 1.3 - Read CSV and make import and add Yelp Data
-    apData = pd.read_csv('./Data/CleanHnetData.csv')
-    apData = updateDfWithYelpDetails(apData, 0, len(apData))
-
-    # # Step 1.4 - Include all rows with column value within X % of data and above Y value
-    apData = removeOutliers(apData, 0.80, 0, 'Price')
-    apData = removeOutliers(apData, 0.80, 100, 'Rent')
-
-    # # Step 1.5 - Read final datafile, and use as dataframe
-    # apData = pd.read_csv('./yelpDataGather.csv')
-
-    # # Step 1.6 - Add PricePerKvm Column
-    apData = addPricePerSizeColumn(apData, 0, len(apData))
-
-    # # Step 1.7 - Remove coordinates that are outside Stockholms municipal
-    apData = removeWrongCoordinates(apData)
-    apData.to_csv('./Data/FilteredApData.csv', index=False)
-
 def main():
 
-    # Step 1 - Run preProcess who gets, cleans, and processes data used. Outputs file FilteredApData.csv
-    #
+    # Run an initial preProcess which gets, cleans & adds geocoding
     preProcess()
-
-    # # Step 2 - Read final datafile, and use as dataframe
-    apData = pd.read_csv('./Data/FilteredApData.csv')
-
-    # # Step 3 - Get a trained model based on dataframe
-    featuresToTrainOn = ['Size', 'NearbyPOIs', 'Latitude', 'Longitude']
-    target = 'Price'
-
-    # Predictions is array with predictions
-    predictions, valPredictionTarget, model = getPredictions(
-        apData, featuresToTrainOn, target)
-
-    print(apData.info())
-    print(apData.head())
-
-    # Step 4 - Print predictions accuracy
-    printMeanAbsoluteError(predictions, valPredictionTarget)
-    printMeanAbsolutePercentageError(predictions, valPredictionTarget)
-
-    # Step 5 - Visualize output data
-
-    # plotThreeDimensionsGraph(
-    #    apData[['Latitude', 'Longitude', 'PricePerKvm']])
-    #plot3DWireframe(apData[['Latitude', 'Longitude', 'PricePerKvm']])
-
-    #plotPredictionsTowardsActual(valPredictionTarget, predictions)
-
 
 main()
